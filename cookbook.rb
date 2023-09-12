ff28d2f36a1b3842ba7eb5c3237c7ed14a5d8fda
oAnthonyG <anthonytguilherme@hotmail.com>
2023-09-12 18:47:46 -0300

    first code in ruby

---
+++ cookbook.rb
@@ -0,0 +1,83 @@
+INSERIR_RECEITA = 1
+VISUALIZAR_RECEITAS = 2
+BUSCAR_RECEITAS = 3
+SAIR = 4
+
+
+puts "Bem vindo ao Cookbook, sua rede social de receitas"
+
+
+
+def menu()
+  puts "[#{INSERIR_RECEITA}] Cadastrar uma receita"
+  puts "[#{VISUALIZAR_RECEITAS}] Ver todas as receitas"
+  puts "[#{BUSCAR_RECEITAS}] Buscar receitas"
+  puts "[#{SAIR}] Sair"
+  
+  print "Escolha uma opção: "
+  return gets.to_i()
+end
+
+def inserir_receita()
+  puts "Digite o nome da receita: "
+  nome = gets.chomp()
+  puts "Digite o tipo da receita: "
+  tipo = gets.chomp()
+  puts
+  puts "Receita #{nome} cadastrada com sucesso!"
+  puts
+  return {nome: nome, tipo: tipo}
+end
+
+def imprimir_receitas(receitas)
+  puts "========== Receitas cadastradas =========="
+  receitas.each do |receita|
+    puts "#{receita[:nome]} - #{receita[:tipo]}"
+  end
+  puts
+  if( receitas.empty?) 
+    puts "Lista de receitas vazia!"
+  end 
+end
+
+def buscar_receitas(receitas)
+  puts "Digite o nome da receita: "
+  nome = gets.chomp()
+
+  receita_encontrada = false
+
+  receitas.each do |receita|
+    if (nome == receita[:nome])
+      puts
+      puts "#{receita[:nome]} esta na lista de receitas"
+      receita_encontrada = true
+    end
+  end 
+  puts
+   unless receita_encontrada
+    puts "Receita não encontrada na lista de receitas!"
+    puts
+   end
+end
+
+receitas = []
+
+opcao = menu()
+
+loop do
+  if(opcao == INSERIR_RECEITA)
+    receitas << inserir_receita()
+  elsif(opcao == VISUALIZAR_RECEITAS)
+    imprimir_receitas(receitas)
+  elsif(opcao == BUSCAR_RECEITAS)
+    buscar_receitas(receitas)
+  elsif(opcao == SAIR)
+    break
+  else
+    puts "Opção inválida"
+  end
+ opcao = menu()
+end
+
+puts
+puts 'Obrigado por usar o CookBook!'
