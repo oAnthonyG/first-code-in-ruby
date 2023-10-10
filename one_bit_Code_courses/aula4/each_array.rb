1523013397ae5ca02e2447142e2f0492833398c0
oAnthonyG <anthonytguilherme@hotmail.com>
2023-10-10 17:16:15 -0300

    more "aulas" for learning ruby

---
+++ one_bit_Code_courses/aula4/each_array.rb
@@ -0,0 +1,12 @@
+ names = ['Joãozinho', 'Manoel', 'Juca']
+
+ name = 'Tony Scorza'
+
+ number = 1
+ names.each do |name|
+  puts number.to_s + name
+  number += 1
+   
+ end
+
+ puts name
---
+++ one_bit_Code_courses/aula4/select_hash.rb
@@ -0,0 +1,8 @@
+hash = {0 => 'zero', 1 => 'um', 2 => 'dois', 3 => 'tres'}
+
+puts 'Selecionando keys com valor maior que 0'
+selection_key = hash.select do |key, valor|
+  key > 0
+end
+
+puts selection_key
