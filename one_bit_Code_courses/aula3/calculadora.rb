9904a444ae3cf69d291590682cc7004463e51e9f
oAnthonyG <anthonytguilherme@hotmail.com>
2023-10-11 16:44:03 -0300

    refactoring calculadora in aula3

--- one_bit_Code_courses/aula3/calculadora.rb
+++ one_bit_Code_courses/aula3/calculadora.rb
@@ -2,37 +2,38 @@ result = ''
 
 loop do 
   puts result
-  puts 'Selecione uma das seguintes opções'
-  puts '1 - Para Somar'
-  puts '2 - Para Subtrair'
-  puts '3 - Para Multiplicação'
-  puts '4 - Para Dividir'
+  puts 'Bem-vindo à Claculadora Simples em Ruby'
+  
+  print 'Por favor, insira o primeiro número: '
+  numero1 = gets.chomp.to_i
+  print 'Agora, insira o segundo número: '
+  numero2 = gets.chomp.to_i
+
+  puts 'Escolha uma operação'
+  puts '1 - Adição'
+  puts '2 - Subtração'
+  puts '3 - Multiplicação'
+  puts '4 - Divisão'
   puts '0 - Para sair'
   puts 'Opções: '
-
   option = gets.chomp.to_i
   if option == 0
+    puts 'Obrigado por usar, tenha uma boa vida!'
     break
   end
 
-    puts 'Digite o primeiro numero: '
-    numero1 = gets.chomp.to_i
-    puts 'Digite o segundo numero: '
-    numero2 = gets.chomp.to_i
-
   if option == 1
-    result = "A Soma dos numeros digitados foi: #{numero1 + numero2}"
+    result = "O resltado da adição de #{numero1} e #{numero2} é: #{numero1 + numero2}"
 
       elsif option == 2  
-        result = "A Divisão dos numeros digitados foi: #{numero1 - numero2}"
+        result = "O resltado da subtração de #{numero1} e #{numero2} é: #{numero1 - numero2}"
 
       elsif option == 3
-        result =  "A Multiplicação dos numeros digitados foi: #{numero1 * numero2}"
+        result =  "O resltado da multiplicação de #{numero1} e #{numero2} é: #{numero1 * numero2}"
 
       elsif option == 4
-        result = "A Divisão dos numeros digitados foi: #{numero1 / numero2}"
+        result = "O resltado da divisão de #{numero1} e #{numero2} é: #{numero1 / numero2}"
 
       else result = 'Opção invalida'
   end
-  system "clear"
 end
