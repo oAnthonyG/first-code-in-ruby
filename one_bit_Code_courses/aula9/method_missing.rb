4684f0d112c31eb258f2b39a2e68b06285c02e03
oAnthonyG <anthonytguilherme@hotmail.com>
2023-10-26 16:27:26 -0300

    method missing

---
+++ one_bit_Code_courses/aula9/method_missing.rb
@@ -0,0 +1,12 @@
+class Fish
+  def method_missing(method_name)
+    puts "Fish don't have #{method_name} behavior"
+  end
+  def swin
+    puts 'Fish is swimming'
+  end
+end
+
+fish = Fish.new
+fish.swin
+fish.walk
